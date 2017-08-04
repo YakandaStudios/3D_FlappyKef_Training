20774fcd1c6a769d70d4c20414553c694cc8a964
chema131 <kevin.gdl.131@gmail.com>
2017-08-04 15:13:24 -0500

    adding repeater

--- Scenario/floor.gd
+++ Scenario/floor.gd
@@ -4,19 +4,24 @@ var camera = null
 var floor_scene = load("res://Scenario/Floor.tscn") 
 var new_floor = null
 var position_camera = null
+var limit_repeater = 50
+var timer = null
 
 func _ready():
 	camera = get_node("../../Camera")
 	
 func _process(delta):
 	position_camera = camera.get_transform().origin
-	if(position_camera.z == 0):
+
+	
+	if( (fmod( round(abs(position_camera.z)), limit_repeater ) )  == 0):
 		repeat_floor()
 		
 func repeat_floor():
-	new_floor =  floor_scene.instance()
-	add_child(new_floor)
-	new_floor.set_transalation(0,0,146)
+	print("repeater")
+#	new_floor =  floor_scene.instance()
+#	add_child(new_floor)
+#	new_floor.set_transalation(0,0,146)
 
 	
 	
--- Scenario/obstacule.gd
+++ Scenario/obstacule.gd
@@ -4,6 +4,5 @@ func _ready():
 	pass
 	
 func _on_Obstacule_body_entered( body ):
-	print(body)
 	if( body.is_in_group("player")):
-		body.die() # replace with function body
+		body.die() 
