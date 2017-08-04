99b4c96246240fdb1cb0b2c7fe7cff30d275b82c
chema131 <kevin.gdl.131@gmail.com>
2017-08-04 12:51:29 -0500

    Updates 4/8/17

--- Flappy/flappyCharacter.gd
+++ Flappy/flappyCharacter.gd
@@ -1,29 +1,22 @@
 extends KinematicBody
 
-export(float) var hor_speed = 10
-export(int) var jump_speed = 2
-export(float) var g = -2
-var dead = false;
-var active = true;
-export(Vector3) var dir = Vector3();
+export(float)  var hor_speed = 0.1
+export(float) var gravity = -9.8
+var dir = Vector3()
 
 func die():
-	queue_free()
+	get_tree().set_pause(true)
+	#queue_free()
 
 func _ready():
 	set_process(true)
 	pass
 		
-func _process(delta):
-	
-	
-	dir.y += delta*(-1.5)
+func _process(delta):	
+	dir.y += delta*gravity
 	dir.z = hor_speed
-	
+
 	if Input.is_action_just_pressed("ui_jump"):
 		dir.y = 0.5
 	
-	move(dir)	
-	
-
-	
+	move(dir)
