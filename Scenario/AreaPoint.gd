138bceb6572cdecf8c8893f2e63fd68d5f03476b
chema131 <kevin.gdl.131@gmail.com>
2017-08-07 14:10:15 -0500

    Final Repeater

--- Scenario/AreaPoint.gd
+++ Scenario/AreaPoint.gd
@@ -1,13 +1,12 @@
 extends Area
 
 var score = 0
-signal puntoNuevo(puntaje)
-var puntaje = null
+var HUD = null
 
 func _ready():
-	puntaje = get_node("../../../HUD")
+	HUD = get_node("../../../HUD")
 
 func _on_punto_body_entered( body ):
 	if( body.is_in_group("player")):
-		score+=1
-		puntaje.set_text(str(score))
+		score += 1
+		HUD.set_text(str(score))
--- Scenario/Scenario.gd
+++ Scenario/Scenario.gd
@@ -4,46 +4,55 @@ extends Node
 var camera = null
 var position_camera = null
 var limit_repeater = 50
+var timer = null
+var timeSolution = false
 
 #New Objects variables
-var new_obstacule = null
-var new_floor = null
-var next_position = Vector3(0,0,5)
+var next_position = Vector3(0,0,0)
+var scale_floor = Vector3(120,10,250)
 
-onready var scene_obstacule = preload("res://Scenario/Obstacules.tscn")
-onready var scene_floor = preload("res://Scenario/Floor.tscn")
+var scene_obstacule = preload("res://Scenario/Obstacules.tscn")
+var scene_floor = preload("res://Scenario/Floor.tscn")
 
-func _ready():
+func _ready():	
 	camera = get_node("../Camera")
 	#first objects to create a scenario
 	var scene_obstacule_instance = scene_obstacule.instance()
 	add_child(scene_obstacule_instance)
 	var scene_floor_instance = scene_floor.instance()
+	scene_floor_instance.set_scale(scale_floor)
 	add_child(scene_floor_instance)
+	timer = Timer.new()
+	timer.set_wait_time(3)
+	timer.connect("timeout", self, "repeater")
+	add_child(timer) 
+
 	
 #The camera moves in relation at the traslation of the camera, this involves the creation of the scenario too.
 func _process(delta):
 	position_camera = camera.get_transform().origin
-	if( (fmod( round(abs(position_camera.z)), limit_repeater ) )  == 0):
-		repeat_floor()
-		repeat_osbtacule()
-		
-func repeat_floor(z_distance):
-	print("repeater")
+	if(timeSolution == false):
+		if( (fmod( round(abs(position_camera.z+1)), limit_repeater ) )  == 0):
+			timer.start()
+
+#			timeSolution = true
+	
+func repeater():
+	next_position.z += 75
+
+	repeat_osbtacule(next_position)
+	repeat_floor(next_position)
 	
 func repeat_osbtacule(z_distance):
-	var scene_instance = scene.instance()
-	add_child(scene_instance)
-
-
-#	add_child(scene_instance)
-#	scenario.set_traslation(trasladar)
-#	new_obstacule =  obstacules_scene.instance()
-#	new_obstacule.set_translation(trasladar)
-#	new_obstacule.set_pos(40,40,146)
-#	scenario.add_child(new_obstacule) # add i
-#	new_floor =  floor_scene.instance()
-#	add_child(new_floor)
-#	new_floor.set_transalation(0,0,146)
+	var scene_obstacule_instance = scene_obstacule.instance()
+	scene_obstacule_instance.translate(z_distance)
+	add_child(scene_obstacule_instance)
 	
-	
+func repeat_floor(z_distance):
+	var scene_floor_instance = scene_floor.instance()
+#	z_distance.x = z_distance.z
+#	z_distance.z = 0
+	print(z_distance)
+	scene_floor_instance.translate(z_distance)
+	scene_floor_instance.set_scale(scale_floor)
+	add_child(scene_floor_instance)
