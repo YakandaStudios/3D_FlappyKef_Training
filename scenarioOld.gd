b8f2d2d37192920fd4ce3d41ba971631dcdc310c
chema131 <kevin.gdl.131@gmail.com>
2017-08-08 14:14:35 -0500

    improve of code of repeater

--- scenarioOld.gd
+++ scenarioOld.gd
@@ -5,9 +5,10 @@ var camera = null
 var position_camera = null
 var limit_repeater_floor = 50
 var limit_repeater_obstacule = 30
-var floor_pos = 0
-var obs_pos = 0
-var difference = 0
+var timerObstacule = null
+var timerFloor = null
+var last_pos = 0
+var new_pos = 0
 
 #New Objects variables
 var next_position_obstacule = Vector3(0,0,0)
@@ -26,30 +27,39 @@ func _ready():
 	var scene_floor_instance = scene_floor.instance()
 	scene_floor_instance.set_scale(scale_floor)
 	add_child(scene_floor_instance)
-
+	#timer 
+	timerObstacule = Timer.new()
+	timerObstacule.set_wait_time(1)
+	timerObstacule.connect("timeout", self, "repeater_obstacule")
+	add_child(timerObstacule) 
+	#timer2
+	timerFloor = Timer.new()
+	timerFloor.set_wait_time(3)
+	timerFloor.connect("timeout", self, "repeater_floor")
+	add_child(timerFloor) 
 	for i in range(0, get_child_count()):
 #    	get_parent().get_child(i).queue_free()
     	print( get_parent().get_parent() )
-	start_pos = camera.get_transform().origin.z
 	
 #The camera moves in relation at the traslation of the camera, this involves the creation of the scenario too.
 func _process(delta):
+
 	position_camera = camera.get_transform().origin
-	print( abs( ( start_pos - round(position_camera.z)) ) )
-	difference =  abs( ( start_pos - round(position_camera.z)) )
-	
-	if ( difference == limit_repeater_obstacule):
-		start_pos = round(position_camera.z)
-		repeater_obstacule()
-	elif (difference == limit_repeater_floor):
-		repeater_floor()
+	if( (fmod( round(abs(position_camera.z+1)), limit_repeater_obstacule ) )  == 0):
+		print(round(abs(position_camera.z+1)))
+		timerObstacule.start()
+	if( (fmod( round(abs(position_camera.z+1)), limit_repeater_floor ) )  == 0):
+		timerFloor.start()
+#	if( (fmod( round(abs(position_camera.z+1)), 100 ) )  == 0):
+#		print( get_tree().get_root() 
+
 	
 func repeater_obstacule():
-	next_position_obstacule.z += 50
+	next_position_obstacule.z += 15
 	osbtacule_flappy(next_position_obstacule)
 	
 func repeater_floor():
-	next_position_floor.z += 50
+	next_position_floor.z += 175
 	floor_flappy(next_position_floor)
 	
 func osbtacule_flappy(z_distance):
